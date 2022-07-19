61030afa577fd6149a81537468e5cd75b4db0815
Andrew <fincheran@gmail.com>
2022-07-19 23:23:42 +0300

    added small fixes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -25,7 +25,7 @@ export class AuthService {
     if (!user) {
       throw new UnauthorizedException('User not found!');
     }
-    if(user?.type === 'user'){
+    if (user?.type === 'user') {
       throw new UnauthorizedException('User doesnt have access!');
     }
     const authTg = await checkTgAuth({
